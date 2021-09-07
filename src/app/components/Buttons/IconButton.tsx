25eac7bd766f05364c0d41414a6c2b2f906f514b
aronvaupel <vaupelaron@gmail.com>
2021-09-07 14:32:46 +0200

    🎨  Implement suggestions from CR + add all icons

--- src/app/components/Buttons/IconButton.tsx
+++ src/app/components/Buttons/IconButton.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import styles from './IconButton.module.css';
 import SwapIcon from '../assets/SwapIcon';
+import AddIcon from '../assets/AddIcon';
+import CallIcon from '../assets/CallIcon';
+import HandshakeIcon from '../assets/HandshakeIcon';
+import HomeIcon from '../assets/HomeIcon';
+import MailIcon from '../assets/MailIcon';
+import NextIcon from '../assets/NextIcon';
+import PreviousIcon from '../assets/PreviousIcon';
+import TrashbinIcon from '../assets/TrashbinIcon';
 
 export type IconButtonProps = {
-  iconType: 'swap';
+  iconType:
+    | 'swap'
+    | 'add'
+    | 'call'
+    | 'handshake'
+    | 'home'
+    | 'mail'
+    | 'next'
+    | 'previous'
+    | 'trashbin';
   onButtonClick: () => void;
   isActive: boolean;
 };
@@ -24,9 +42,21 @@ const IconButton = ({
 
   const iconStateMap = {
     swap: <SwapIcon {...(isActive ? active : inactive)} />,
+    add: <AddIcon {...(isActive ? active : inactive)} />,
+    call: <CallIcon {...(isActive ? active : inactive)} />,
+    home: <HomeIcon {...(isActive ? active : inactive)} />,
+    next: <NextIcon {...(isActive ? active : inactive)} />,
+    previous: <PreviousIcon {...(isActive ? active : inactive)} />,
+    trashbin: <TrashbinIcon {...(isActive ? active : inactive)} />,
+    handshake: <HandshakeIcon {...(isActive ? active : inactive)} />,
+    mail: <MailIcon {...(isActive ? active : inactive)} />,
   };
 
-  return <a onClick={onButtonClick}>{iconStateMap[iconType]}</a>;
+  return (
+    <button className={styles.button} onClick={onButtonClick}>
+      {iconStateMap[iconType]}
+    </button>
+  );
 };
 
 export default IconButton;
