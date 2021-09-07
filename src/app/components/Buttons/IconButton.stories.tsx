8a3083b00d28ffcd0c66e9b5659d9cb8c635bbec
aronvaupel <vaupelaron@gmail.com>
2021-09-07 14:34:40 +0200

    ✨ Add missing icons to storybook

--- src/app/components/Buttons/IconButton.stories.tsx
+++ src/app/components/Buttons/IconButton.stories.tsx
@@ -8,7 +8,17 @@ export default {
   component: IconButton,
   argTypes: {
     iconType: {
-      options: ['swap'],
+      options: [
+        'swap',
+        'add',
+        'call',
+        'mail',
+        'next',
+        'previous',
+        'home',
+        'trashbin',
+        'handshake',
+      ],
       control: { type: 'select' },
     },
   },
@@ -19,5 +29,53 @@ const Template: Story<IconButtonProps> = (args) => <IconButton {...args} />;
 export const SwapIcon = Template.bind({});
 SwapIcon.args = {
   iconType: 'swap',
+  isActive: true,
+};
+
+export const AddIcon = Template.bind({});
+AddIcon.args = {
+  iconType: 'add',
+  isActive: true,
+};
+
+export const CallIcon = Template.bind({});
+CallIcon.args = {
+  iconType: 'call',
+  isActive: true,
+};
+
+export const MailIcon = Template.bind({});
+MailIcon.args = {
+  iconType: 'mail',
+  isActive: true,
+};
+
+export const TrashbinIcon = Template.bind({});
+TrashbinIcon.args = {
+  iconType: 'trashbin',
+  isActive: true,
+};
+
+export const HandshakeIcon = Template.bind({});
+HandshakeIcon.args = {
+  iconType: 'handshake',
+  isActive: true,
+};
+
+export const NextIcon = Template.bind({});
+NextIcon.args = {
+  iconType: 'next',
+  isActive: true,
+};
+
+export const HomeIcon = Template.bind({});
+HomeIcon.args = {
+  iconType: 'home',
+  isActive: true,
+};
+
+export const PreviousIcon = Template.bind({});
+PreviousIcon.args = {
+  iconType: 'previous',
   isActive: true,
 };
