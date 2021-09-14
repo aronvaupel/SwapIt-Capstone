6df4151dc082930afcfc654d458e23b5496665b6
aronvaupel <vaupelaron@gmail.com>
2021-09-14 13:49:28 +0200

    🎨 Create addPage

---
+++ src/app/pages/AddPage/AddPage.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import styles from './AddPage.module.css';
+import Header from '../../components/Header/Header';
+import Nav from '../../components/Nav/Nav';
+import Input from '../../components/inputFields/Input';
+import RatingInput from '../../components/Rating/RatingInput';
+import DescriptionField from '../../components/inputFields/DescriptionField';
+import ActionButton from '../../components/ActionButton/ActionButton';
+
+function AddPage(): JSX.Element {
+  const handleValueClick = () => {
+    console.log('Value');
+  };
+
+  const handleClickPhoto = () => {
+    console.log('Photo');
+  };
+  const handleClickSubmit = () => {
+    console.log('Submit');
+  };
+
+  const valueInput = 1;
+
+  return (
+    <div className={styles.wrapper}>
+      <Header />
+      <main className={styles.mainWrapper}>
+        <Input
+          label="What do you want to swap?"
+          id="name"
+          placeholder="Enter a name for your item"
+        />
+        <RatingInput
+          type="value"
+          value={valueInput}
+          onValueClick={handleValueClick}
+        />
+        <RatingInput
+          type="condition"
+          value={valueInput}
+          onValueClick={handleValueClick}
+        />
+        <DescriptionField
+          label="Description"
+          placeholder="Enter information about your item"
+          id="description"
+        />
+        <ActionButton value="Upload photo" onClick={handleClickPhoto} />
+        <ActionButton value="Submit" onClick={handleClickSubmit} />
+      </main>
+      <Nav />
+    </div>
+  );
+}
+export default AddPage;
