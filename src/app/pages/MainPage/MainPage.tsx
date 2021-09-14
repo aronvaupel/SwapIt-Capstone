f6dd7a131fb646b14c31ae0fcd0ae89828f6e4cd
aronvaupel <vaupelaron@gmail.com>
2021-09-14 17:43:24 +0200

    ✨ Add main page

---
+++ src/app/pages/MainPage/MainPage.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import styles from './MainPage.module.css';
+import Header from '../../components/Header/Header';
+import Nav from '../../components/Nav/Nav';
+import MainCard from '../../components/mainCard/mainCard';
+import IconButton from '../../components/Buttons/IconButton';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Carousel } from 'react-responsive-carousel';
+
+function MainPage(): JSX.Element {
+  const mockupData = [
+    {
+      src: 'src/app/components/assets/1.jpg',
+      value: 1,
+      condition: 2,
+    },
+    {
+      src: 'src/app/components/assets/1.jpg',
+      value: 2,
+      condition: 3,
+    },
+    {
+      src: 'src/app/components/assets/1.jpg',
+      value: 3,
+      condition: 4,
+    },
+    {
+      src: 'src/app/components/assets/1.jpg',
+      value: 4,
+      condition: 5,
+    },
+    {
+      src: 'src/app/components/assets/1.jpg',
+      value: 1,
+      condition: 5,
+    },
+  ];
+
+  const handleClick = () => {
+    console.log('click');
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <Header />
+      <main className={styles.mainWrapper}>
+        <p>Your offer</p>
+        <Carousel infiniteLoop={true} showThumbs={false}>
+          {mockupData.map((item) => (
+            <MainCard
+              imageSrc={item.src}
+              ratingValue={item.value}
+              ratingCondition={item.condition}
+              {...item}
+            />
+          ))}
+        </Carousel>
+        <p>Somebody´s offer</p>
+        <Carousel infiniteLoop={true} showThumbs={false}>
+          {mockupData.map((item) => (
+            <MainCard
+              imageSrc={item.src}
+              ratingValue={item.value}
+              ratingCondition={item.condition}
+              {...item}
+            />
+          ))}
+        </Carousel>
+        <div className={styles.iconBackground}>
+          <IconButton iconType="swap" isActive={true} onClick={handleClick} />
+        </div>
+      </main>
+      <Nav />
+    </div>
+  );
+}
+export default MainPage;
