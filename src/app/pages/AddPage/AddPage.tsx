2264a8e43f44a1f435aa0d1ef366032aa3e3c179
aronvaupel <vaupelaron@gmail.com>
2021-09-15 13:47:24 +0200

    🎨 Make input controlled and validate inputs

--- src/app/pages/AddPage/AddPage.tsx
+++ src/app/pages/AddPage/AddPage.tsx
@@ -6,10 +6,27 @@ import Input from '../../components/inputFields/Input';
 import RatingInput from '../../components/Rating/RatingInput';
 import DescriptionField from '../../components/inputFields/DescriptionField';
 import ActionButton from '../../components/ActionButton/ActionButton';
+import { useState } from 'react';
 
 function AddPage(): JSX.Element {
-  const handleValueClick = () => {
-    console.log('Value');
+  const [itemName, setItemname] = useState('');
+  const [valueInput, setValueInput] = useState(0);
+  const [conditionInput, setConditionInput] = useState(0);
+  const [description, setDescription] = useState('');
+
+  const invalidItemName = () => itemName.length < 3;
+  const invalidValueInput = () => valueInput <= 0;
+  const invalidConditionInput = () => conditionInput <= 0;
+
+  const isDisabled =
+    invalidItemName() || invalidValueInput() || invalidConditionInput();
+
+  const handleValueClick = (value: number) => {
+    setValueInput(value);
+  };
+
+  const handleConditionClick = (value: number) => {
+    setConditionInput(value);
   };
 
   const handleClickPhoto = () => {
@@ -19,37 +36,51 @@ function AddPage(): JSX.Element {
     console.log('Submit');
   };
 
-  const valueInput = 1;
-
   return (
     <div className={styles.wrapper}>
       <Header />
       <main className={styles.mainWrapper}>
         <div className={styles.elements}>
-          <Input
-            label="What do you want to swap?"
-            id="name"
-            placeholder="Enter a name for your item"
-          />
-          <RatingInput
-            type="value"
-            value={valueInput}
-            onValueClick={handleValueClick}
-          />
-          <RatingInput
-            type="condition"
-            value={valueInput}
-            onValueClick={handleValueClick}
-          />
-          <DescriptionField
-            label="Description"
-            placeholder="Enter information about your item"
-            id="description"
-          />
-          <ActionButton value="Upload photo" onClick={handleClickPhoto} />
-          <div className={styles.submit}>
-            <ActionButton value="Submit" onClick={handleClickSubmit} />
-          </div>
+          <form>
+            <Input
+              label="What do you want to swap?"
+              id="name"
+              placeholder="Enter a name for your item"
+              inputType="text"
+              value={itemName}
+              onChange={(value) => setItemname(value)}
+            />
+            <RatingInput
+              type="value"
+              value={valueInput}
+              onValueClick={handleValueClick}
+            />
+            <RatingInput
+              type="condition"
+              value={conditionInput}
+              onValueClick={handleConditionClick}
+            />
+            <DescriptionField
+              label="Description"
+              placeholder="Enter information about your item"
+              id="description"
+              value={description}
+              onChange={(value) => setDescription(value)}
+            />
+            <ActionButton
+              value="Upload photo"
+              onClick={handleClickPhoto}
+              isActive={true}
+            />
+            <div className={styles.submit}>
+              <ActionButton
+                value="Submit"
+                onClick={handleClickSubmit}
+                disabled={isDisabled}
+                isActive={!isDisabled}
+              />
+            </div>
+          </form>
         </div>
       </main>
       <Nav />
