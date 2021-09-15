fa580523791116767f4e2edb2f541eeb70fd7937
aronvaupel <vaupelaron@gmail.com>
2021-09-15 12:24:38 +0200

    🎨 Add form and make Inputs controlled components

--- src/app/pages/LogInPage/LogInPage.tsx
+++ src/app/pages/LogInPage/LogInPage.tsx
@@ -4,22 +4,54 @@ import Header from '../../components/Header/Header';
 import Nav from '../../components/Nav/Nav';
 import Input from '../../components/inputFields/Input';
 import ActionButton from '../../components/ActionButton/ActionButton';
+import { useState } from 'react';
 
 function LogInPage(): JSX.Element {
-  const handleClick = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const validUsername = () => username.length < 3;
+  const validPassword = () => password.length < 8;
+
+  const isDisabled = validUsername() || validPassword();
+
+  const handleSubmit = () => {
     console.log('Click');
   };
   return (
     <div className={styles.wrapper}>
       <Header />
       <main className={styles.mainWrapper}>
-        <div className={styles.input}>
-          <Input placeholder="Enter username" label="Username" id="username" />
-          <Input placeholder="Enter password" label="Password" id="password" />
-        </div>
-        <div className={styles.action}>
-          <ActionButton type="submit" value="Submit" onClick={handleClick} />
-        </div>
+        <form className={styles.form}>
+          {' '}
+          <div className={styles.input}>
+            <Input
+              placeholder="Enter username"
+              label="Username"
+              id="username"
+              inputType="text"
+              value={username}
+              onChange={(value) => setUsername(value)}
+            />
+            <Input
+              placeholder="Enter password"
+              label="Password"
+              id="password"
+              inputType="password"
+              value={password}
+              onChange={(value) => setPassword(value)}
+            />
+          </div>
+          <div className={styles.action}>
+            <ActionButton
+              type="submit"
+              value="Submit"
+              onClick={handleSubmit}
+              disabled={isDisabled}
+              isActive={!isDisabled}
+            />
+          </div>
+        </form>
       </main>
       <Nav />
     </div>
