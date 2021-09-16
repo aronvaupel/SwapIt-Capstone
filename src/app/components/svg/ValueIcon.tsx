d6ea937848d74d151aa66fbb8b609a73391c4936
aronvaupel <vaupelaron@gmail.com>
2021-09-16 13:17:55 +0200

    💄 Improve consition- and Value-Icon

--- src/app/components/svg/ValueIcon.tsx
+++ src/app/components/svg/ValueIcon.tsx
@@ -3,12 +3,11 @@ import React from 'react';
 const ValueIcon = (props: React.SVGProps<SVGSVGElement>): JSX.Element => {
   return (
     <svg
-      width="14"
-      height="14"
-      viewBox="0 1 18 16"
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      {...props}
     >
       <circle cx="8" cy="8" r="7.5" fill="#F5F5F5" stroke="#292929" />
       <path
