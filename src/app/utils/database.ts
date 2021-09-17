03dfe62d0db2869c0d2b9d79e675bdb8f11d77dd
aronvaupel <vaupelaron@gmail.com>
2021-09-17 16:37:04 +0200

    ✨ Create basic database helper functions

---
+++ src/app/utils/database.ts
@@ -0,0 +1,18 @@
+import { MongoClient } from 'mongodb';
+import type { Collection } from 'mongodb';
+import type { User } from './types';
+
+let client: MongoClient;
+
+export async function connectDatabase(url: string): Promise<void> {
+  client = new MongoClient(url);
+  await client.connect();
+}
+
+export function getCollection<T>(name: string): Collection<T> {
+  return client.db().collection<T>(name);
+}
+
+export function getUserCollection(): Collection<User> {
+  return getCollection<User>('users');
+}
