3eef0a99dc638b7294d3bf7e1464fcf42cbbdc44
aronvaupel <vaupelaron@gmail.com>
2021-09-17 16:37:50 +0200

    🎨 Add post route

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,36 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import { createUser } from './app/utils/CRUDfunctions';
+import type { User } from './app/utils/types';
+import { connectDatabase } from './app/utils/database';
 
 const app = express();
 const port = process.env.PORT || 3001;
+
 app.use(express.json());
+
+app.post('/api/users', async (req, res) => {
+  const user: User = req.body;
+  await createUser(user);
+  return res.status(200).send(user);
+});
 
 app.use('/storybook', express.static('dist/storybook'));
 
-app.get('/api/hello', (_request, response) => {
-  response.send('Hello API!');
-});
-
 app.use(express.static('dist/app'));
+
 app.get('*', (_request, response) => {
   response.sendFile('index.html', { root: 'dist/app' });
 });
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}!`);
+if (!process.env.MONGODB_URL) {
+  throw new Error('No mongo database available!');
+}
+
+connectDatabase(process.env.MONGODB_URL).then(() => {
+  console.log('connected to db');
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  });
 });
