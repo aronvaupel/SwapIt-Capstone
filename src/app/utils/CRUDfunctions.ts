a316f347a6bc619cc9ad7fbb6debed061a2dbf7a
aronvaupel <vaupelaron@gmail.com>
2021-09-18 12:35:16 +0200

    🎨 File renamed

--- src/app/utils/CRUDfunctions.ts
+++
@@ -1,7 +0,0 @@
-import type { User } from './types';
-import { getUserCollection } from './database';
-
-export async function createUser(user: User): Promise<void> {
-  const users = getUserCollection();
-  users.insertOne(user);
-}
