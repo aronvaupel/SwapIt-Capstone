58184db5088e7621bd2c31cf657899d94b91e316
aronvaupel <vaupelaron@gmail.com>
2021-09-20 13:06:13 +0200

    ✨ Add getUser function

--- src/app/utils/users.ts
+++ src/app/utils/users.ts
@@ -5,3 +5,9 @@ export async function createUser(user: User): Promise<void> {
   const users = getUserCollection();
   users.insertOne(user);
 }
+
+export async function getUser(username: string): Promise<User | null> {
+  const userCollection = getUserCollection();
+  const user = await userCollection.findOne({ username });
+  return user;
+}
