8c645e06b54a255df9538b932d58e20b44db8bc9
aronvaupel <vaupelaron@gmail.com>
2021-09-24 11:03:04 +0200

    ✨ Create helper functions to implement swap functionality

---
+++ src/utils/proposals.ts
@@ -0,0 +1,27 @@
+import type { Proposal } from './types';
+import { getProposalCollection } from './database';
+import type { ObjectId } from 'mongodb';
+
+export async function createProposal(proposal: Proposal): Promise<void> {
+  const proposals = getProposalCollection();
+  await proposals.insertOne(proposal);
+}
+
+export async function deleteProposal(proposal: Proposal): Promise<void> {
+  const proposals = getProposalCollection();
+  await proposals.deleteOne(proposal);
+}
+
+export async function getProposal(
+  users: string[],
+  items: string[]
+): Promise<Proposal | null> {
+  const proposals = getProposalCollection();
+  const proposal = await proposals.findOne({ users, items });
+  return proposal;
+}
+
+export async function getOwnProposals(owner: ObjectId): Promise<Proposal[]> {
+  const items = getProposalCollection();
+  return await items.find({ creator: owner }).toArray();
+}
