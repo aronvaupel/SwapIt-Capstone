6ce8dedc8ab795bd0fbd10ce01935e32581aeaed
aronvaupel <vaupelaron@gmail.com>
2021-09-24 11:09:19 +0200

    🎨 Create helper for matchesfunctions needed to implement swap functionality

---
+++ src/utils/matches.ts
@@ -0,0 +1,18 @@
+import type { Match } from './types';
+import { getMatchCollection } from './database';
+import type { ObjectId } from 'mongodb';
+
+export async function createMatch(match: Match): Promise<void> {
+  const matches = getMatchCollection();
+  await matches.insertOne(match);
+}
+
+export async function deleteMatch(match: Match): Promise<void> {
+  const matches = getMatchCollection();
+  await matches.deleteOne(match);
+}
+
+export async function getOwnMatches(user: ObjectId): Promise<Match[]> {
+  const matches = getMatchCollection();
+  return await matches.find({ users: [{ $in: user }] }).toArray();
+}
